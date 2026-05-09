ed40cf6b7ddf21052c55085362ce439ccf49d0b8
matheus90512898-ux <matheus90512898@edu.unipar.br>
2026-05-09 13:55:58 -0300

    Update limiter.py

--- backend/app/core/limiter.py
+++ backend/app/core/limiter.py
@@ -1,4 +1,9 @@
+from fastapi import Request
 from slowapi import Limiter
-from slowapi.util import get_remote_address
 
-limiter = Limiter(key_func=get_remote_address)
+
+def get_real_ip(request: Request) -> str:
+    return request.client.host  # zero trust em headers
+
+
+limiter = Limiter(key_func=get_real_ip)
